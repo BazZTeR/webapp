d063484f2d4ce88802e052884a39ad8b66486576
Your Name <you@example.com>
2018-09-03 23:00:59 +0300

    admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AdminService } from '../services/admin.service';
+import { User } from '../entities/user';
 
 @Component({
-  selector: 'app-admin',
+  selector: 'app-network',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  @ViewChild('f') search: NgForm;
 
-  constructor() { }
+  displaySwitch = true;
+  users: User[] = [];
+
+  constructor(private admin:AdminService) { }
 
   ngOnInit() {
+    this.admin.getAllUsers().subscribe(
+      (users: any[]) => {
+        console.log(users);
+        for(var i in users){
+          console.log(i);
+          console.log(users[i]);
+          console.log(users[i].firstname+ " " +users[i].lastname);
+          this.users[i] = users[i];
+        }
+      }
+    );
+  }
+
+  onSubmit(){
+    console.log(this.search);
+    console.log(this.search.value.search);
+    this.admin.search(this.search.value.search).subscribe(
+      (users: any[]) => {
+        console.log(users);
+        for(var i in users){
+          console.log(i);
+          console.log(users[i]);
+          console.log(users[i].firstname+ " " +users[i].lastname);
+          this.users[i] = users[i];
+        }
+      }
+    );
+    this.displaySwitch = false;
   }
 
 }
