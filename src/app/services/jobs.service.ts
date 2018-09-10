3968b5f0a2ddbfd30b05025f0612c96f646c8c99
Your Name <you@example.com>
2018-09-10 05:24:51 +0300

    jobs

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Job } from '../entities/job';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +7,6 @@ import { Injectable } from '@angular/core';
 export class JobsService {
   
   displaySwitch = true;
-
+  jobs : Job[]=[];
   constructor() { }
 }
